6ca00eac4d005c2ef5a3c2483334bb7b179a1ca4
Mateo <mateo.glavicic@gmail.com>
2021-08-15 11:15:11 +0200

    new class Oglas db context

---
+++ TestSimpleWebApp/Data/TestSimpleWebAppContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestSimpleWebApp.Models;
+
+namespace TestSimpleWebApp.Data
+{
+    public class TestSimpleWebAppContext : DbContext
+    {
+        public TestSimpleWebAppContext(DbContextOptions<TestSimpleWebAppContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Oglas> Oglasi { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Oglas>().ToTable("Oglas");
+        }
+    }
+}
---
+++ TestSimpleWebApp/Models/Oglas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestSimpleWebApp.Models
+{
+    public class Oglas
+    {
+        public int ID { get; set; }
+        public String Naziv { get; set; }
+        public int VlasnikID { get; set; }
+        public String Opis { get; set; }
+    }
+}
