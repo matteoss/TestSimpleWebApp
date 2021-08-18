cb5b92fb2f70f4c030b2c040ae61481b174bd056
Mateo <mateo.glavicic@gmail.com>
2021-08-19 01:23:48 +0200

    search function modified

--- TestSimpleWebApp/Startup.cs
+++ TestSimpleWebApp/Startup.cs
@@ -29,6 +29,7 @@ namespace TestSimpleWebApp
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")); 
             });
             services.AddControllers();
+            //services.AddAp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- TestSimpleWebApp/Static/js/app.index.js
+++ TestSimpleWebApp/Static/js/app.index.js
@@ -1,2 +1,30 @@
 ﻿
 requirejs(['js/common']);
+
+var oglasi;
+var page;
+var hasMorePages;
+var search;
+require(['ko'], function (ko) {
+    oglasi = ko.observableArray(); //[]; // [{naslov: "oglas1", opis: "opis1"}, {naslov: "oglas2", opis: "opis2"}];
+    page = ko.observable(1);
+    search = ko.observable("");
+    hasMorePages = ko.observable(false);
+});
+var refresh_function = function () {
+    require(['ko', 'jquery'], function (ko, $) {
+        oglasi.removeAll();
+        $.getJSON("/Oglasi/" + page() + "/" + search(), function (result) {
+            $.each(result.List, function (i, field) {
+                oglasi.push(field);
+            });
+            hasMorePages(result.HasMore);
+        });
+    });
+}
+refresh_function();
+
+var search_function = function () {
+    page(1);
+    refresh_function();
+}
