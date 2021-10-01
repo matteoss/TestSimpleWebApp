e1bd5f139d36e2e6b312bbf1ffc3d020da81e702
Mateo <mateo.glavicic@gmail.com>
2021-10-01 08:02:13 +0200

    drop down list

--- TestSimpleWebApp/Static/components/drop-down-list.js
+++ TestSimpleWebApp/Static/components/drop-down-list.js
@@ -1,47 +1,99 @@
 ﻿define(['ko', 'jquery', 'text!./drop-down-list.html'], function (ko, $, htmlString) {
     function ddlist(params) {
-        console.log(ko.toJSON(params));
+        console.log("drop-down-list " + ko.toJSON(params));
         let self = this;
         this.componentId = params.componentId;
-        this.id = params.id;
-        this.value = ko.observable().extend({ rateLimit: 400 });
 
+        if (ko.isObservable(params.id)) {
+            this.checkedId = params.id;
+        } else {
+            this.checkedId = ko.observable(params.id);
+        }
+
+        this.valueRateLimitIgnore = false;
+        this.valueRateLimit = function (action, timeout) {
+            var timeoutInstance;
+            var initTimeoutInstance = false;
+            return function () {
+                console.log(" valueRateLimit 0 " + JSON.stringify(timeoutInstance));
+                if (!initTimeoutInstance || self.valueRateLimitIgnore) {
+                    action();
+                    initTimeoutInstance = true;
+                    self.valueRateLimitIgnore = false;
+                } else {
+                    if (timeoutInstance) {
+                        clearTimeout(timeoutInstance);
+                    }
+                    timeoutInstance = setTimeout(function () {
+                        timeoutInstance = undefined;
+                        action();
+                        console.log(" valueRateLimit 2 " +JSON.stringify(timeoutInstance));
+                    }, timeout);
+                }
+            };
+        }
+
+        this.value = ko.observable().extend({ rateLimit: { timeout: 500, method: self.valueRateLimit }  });
+
+        this.valueSkipRefresh = false;
         this.valueSubscription = this.value.subscribe(function (newValue) {
-            self.id = null;
-            console.log("dropdown update: " + newValue);
-            self.refreshFunction();
+            console.log("drop-down-list update: " + newValue);
+            if (!self.valueSkipRefresh) {
+                self.refreshFunction();
+            } else {
+                self.valueSkipRefresh = false;
+            }
+        });
+        this.setValue = function (value) {
+            self.valueSkipRefresh = true;
+            self.valueRateLimitIgnore = true;
+            self.value(value);
+        }
+        this.checkedIdSubscription = this.checkedId.subscribe(function (newValue) {
+            console.log("drop-down-list " + newValue);
+            self.setValue(self.list1().find(element => element.id == newValue).itemText);
         });
 
         this.inputHasFocus = ko.observable(false);
         this.display = ko.pureComputed(function () {
-            if (self.inputHasFocus() /*|| self.list1().findIndex((e) => e.itemHasFocus()) > -1*/) {
+            if (self.inputHasFocus() /*|| self.list1().findIndex((e) => e.itemHasFocus()) > -1*/ || self.scrollDivClicked()) {
                 $(document).ready(function () {
                     $("#" + self.scrollDivId).on('scroll', function () {
                         if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
-                            console.log('end reached');
+                            console.log('drop-down-list end reached');
                             self.loadMoreFunction();
                         }
                         //console.log($(this).scrollTop() + '-' + $(this).innerHeight() + '-' + $(this)[0].scrollHeight);
                     });
-                    console.log("#" + self.scrollDivId);
+                    $('#' + self.scrollDivId).on('mousedown', function () {
+                        self.scrollDivClicked(true);
+                        console.log("drop-down-list scrolldivcliked true");
+                    });
+                    $('#' + self.scrollDivId).on('mouseup', function () {
+                        self.scrollDivClicked(false);
+                        console.log("drop-down-list scrolldivcliked false");
+                    });
+                    console.log("drop-down-list #" + self.scrollDivId);
                 });
                 return true;
             } else {
                 return false;
             }
-        }).extend({ rateLimit: 50 });
-        this.checkedId = ko.observable();
-        this.checkedIdSubscription = this.checkedId.subscribe(function (newValue) {
-            console.log(newValue);
-        });
+        }).extend({ rateLimit: 20 });
+
         this.radioGroupName = "drop-down-list-" + this.componentId;
         this.scrollDivId = "scroll-div-" + this.componentId;
+        this.scrollDivClicked = ko.observable(false);
         this.scrollContentId = "scroll-content-" + this.componentId;
+
         this.list1 = ko.observableArray();
         this.fetchSize = 10;
         this.fetchIteration = 0;
         this.hasMore = true;
-        this.modelName = "Guests";
+        this.modelName = params.modelName;
+        this.searchColumns = params.searchColumns;
+        this.searchRegExp = new RegExp("[a-zA-Z0-9]");
+        this.valueFunction = typeof params.valueFunction === 'function' ? params.valueFunction : (e) => "" + e.id;
 
         this.selected = null;
 
@@ -53,8 +105,16 @@
                 } else if (self.value()) {
                     let ftmp = [];
                     $.each(self.value().split(" "), function (i, e) {
-                        ftmp.push("contains(Surname, '" + e + "')");
-                        ftmp.push("contains(Name, '" + e + "')");
+                        $.each(self.searchColumns, function (j, c) {
+                            if (c === 'id') {
+                                if (!isNaN(parseInt(e))) {
+                                    ftmp.push(c + " eq " + e);
+                                }
+                            } else if (self.searchRegExp.test(e)) {
+                                ftmp.push("contains(" + c + ", '" + e + "')");
+                            }
+                            
+                        });
                     });
                     args.push("$filter="+ftmp.join(" or "));
                 }
@@ -63,7 +123,7 @@
                 args.push("$orderby=Id");
 
                 let query = "/odata/" + self.modelName + "/?" + args.join("&");
-                console.log(query);
+                console.log("drop-down-list"+query);
                 $.getJSON(query, function (result) {
                     //console.log(JSON.stringify(result));
                     //self.guests(result.value);
@@ -73,9 +133,10 @@
 
                         self.list1.push({
                             id: e.id,
-                            itemText: e.id + " - "+e.surname + " " + e.name,
-                            htmlId: self.radioGroupName + '-' + (self.list1.length+1),
-                            itemHasFocus: ko.observable(false),
+                            itemText: self.valueFunction(e),
+                            htmlId: self.radioGroupName + '-' + (self.list1().length+1),
+                            liHtmlId: self.radioGroupName + '-li-' + (self.list1().length+1),
+                            ///itemHasFocus: ko.observable(false),
                             itemClass: ko.observable("")
                         });
 
@@ -104,7 +165,7 @@
             self.list1.removeAll();
             self.fetchIteration = 0;
             self.hasMore = true;
-            this.selected = null;
+            self.selected = null;
             self.selectFunction();
             self.getData();
         }
@@ -112,15 +173,16 @@
         this.loadMoreFunction = function () {
             self.getData();
         }
+
         this.initFunction = function () {
             self.list1.removeAll();
             self.fetchIteration = 0;
             self.hasMore = true;
-            this.selected = null;
+            self.selected = null;
             self.selectFunction();
-            self.getData("id eq " + self.id, () => {
+            self.getData("id eq " + self.checkedId(), () => {
                 if (self.list1().length === 1) {
-                    self.value(self.list1()[0].itemText);
+                    self.setValue(self.list1()[0].itemText);
                 }
             });
         }
@@ -128,12 +190,12 @@
         this.selectFunction = function(){
             $.each(self.list1(), (i, e) => e.itemClass(""));
             if (self.selected != null) {
-                self.list1()[self.selected].itemClass("dd-selected-item");
+                self.list1()[self.selected].itemClass("drop-down-list-selected-item");
             }
         }
 
         this.upDownEvent = function (data, event) {
-            console.log("drop-down-list key down " + JSON.stringify(event.keyCode)); // 38 40
+            //console.log("drop-down-list key down " + JSON.stringify(event.keyCode)); // 38 40
             if (event.keyCode == 38) {
                 if (self.selected > 0) {
                     self.selected--;
@@ -141,6 +203,17 @@
                     self.selected = 0;
                 }
                 self.selectFunction();
+
+                console.log("drop-down-list key down " + $("#" + self.list1()[self.selected].liHtmlId).position().top
+                    + " " + $("#" + self.list1()[self.selected].liHtmlId).innerHeight()
+                    + " " + $("#" + self.scrollDivId).innerHeight()
+                    + " " + $("#" + self.scrollDivId).scrollTop()
+                );
+
+                if ($("#" + self.list1()[self.selected].liHtmlId).position().top < 0) {
+                    $("#" + self.scrollDivId).scrollTop($("#" + self.scrollDivId).scrollTop() + $("#" + self.list1()[self.selected].liHtmlId).position().top);
+                }
+
             } else if (event.keyCode == 40) {
                 if (self.selected != null && self.selected < self.list1().length-1) {
                     self.selected++;
@@ -148,11 +221,24 @@
                     self.selected = 0;
                 }
                 self.selectFunction();
+
+                console.log("drop-down-list key down " + $("#" + self.list1()[self.selected].liHtmlId).position().top
+                    + " " + $("#" + self.list1()[self.selected].liHtmlId).innerHeight()
+                    + " " + $("#" + self.scrollDivId).innerHeight()
+                    + " " + $("#" + self.scrollDivId).scrollTop()
+                );
+
+                if ($("#" + self.list1()[self.selected].liHtmlId).position().top + $("#" + self.list1()[self.selected].liHtmlId).innerHeight() >= $("#" + self.scrollDivId).innerHeight()) {
+                    $("#" + self.scrollDivId).scrollTop($("#" + self.scrollDivId).scrollTop()
+                        + $("#" + self.list1()[self.selected].liHtmlId).position().top + $("#" + self.list1()[self.selected].liHtmlId).innerHeight() - $("#" + self.scrollDivId).innerHeight()
+                    );
+                }
+
             } else if (event.keyCode == 13) {
                 if (self.selected != null) {
                     self.checkedId(self.list1()[self.selected].id);
-                    self.id = self.checkedId();
-                    self.initFunction();
+                    self.setValue(self.list1()[self.selected].itemText);
+                    self.inputHasFocus(false);
                 }
             }
             return true;
@@ -162,12 +248,11 @@
 
 
         $(document).ready(function () {
-            if (self.id) {
+            if (self.checkedId()) {
                 self.initFunction();
             } else {
                 self.refreshFunction();
             }
-            
         });
     }
 
